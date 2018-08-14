955188d616c2fa25bbfd56bc0d2d95634fc33cc2
Yugandhar Reddy Thippireddy <yreddy@Yugandhars-MacBook-Pro.local>
2018-08-14 11:21:41 -0500

    dao and junit

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -1,8 +1,9 @@
 package com.mulesoft.training;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
 
 public class HelloMavenTest extends FunctionalTestCase {
@@ -10,7 +11,16 @@ public class HelloMavenTest extends FunctionalTestCase {
     @Test
     public void mavenFlowReturnsHelloMaven() throws Exception {
         runFlowAndExpect("mavenFlow", "Hello World");
-    }
+    } 
+    
+    @Test
+    public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+    	   MuleEvent event = runFlow("retrieveFlights");
+    	   String contentType = event.getMessage().getOutboundProperty("Content-Type");
+    	   assertEquals("application/json", contentType);
+    	 }
+    
+    
     
     @Override
     protected String getConfigFile() {
